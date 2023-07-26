0565eecf16a9b6801e0380ffad63f56f0ba135e2
Rafael <rafaelguisso@gmail.com>
2023-07-26 18:11:10 -0300

    adicionando os files de f-string e idade

---
+++ #f-string.py
@@ -0,0 +1,7 @@
+#f-string
+
+name = "Rafael"
+age = 30
+country = "Brazil"
+
+print(f"Hello! My name is {name}, i'm {age} years old and i'm from {country}")
---
+++ Idade.py
@@ -0,0 +1,6 @@
+idade = int(input("Informe sua idade"))
+
+if idade >= 18:
+    print("Voce possui idade suficiente para tirar a CNH")
+else:
+    print("Voce nao pode tirar a CNH")
